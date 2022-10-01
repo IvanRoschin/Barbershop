08fbdee7a2882d9c4819bca33f1314e7e47ad77c
Ivan Roschin <ivan.roschin86@gmail.com>
2022-10-01 21:57:21 +0300

    update links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-   
+import agreement from './js/agreement';
+import animation from './js/animations';
+import headerScroll from './js/header-scroll';
+import menu from './js/menu';
+import modal from './js/modal';
